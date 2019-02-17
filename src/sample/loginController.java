62e20367a0e9c4091ae3e54b9134a825b8738b2a
Gabriel Tan <gabtan99@gmail.com>
2019-02-18 00:10:03 +0800

    added option to login as guest

--- src/sample/loginController.java
+++ src/sample/loginController.java
@@ -1,5 +1,15 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class loginController {
 
+	public AnchorPane welcomeAnchor;
+	public AnchorPane registerAnchor;
+
+	public void loginScreen(ActionEvent actionEvent) {
+		welcomeAnchor.setVisible(false);
+		registerAnchor.setVisible(true);
+	}
 }
