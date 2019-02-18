8c1f98fdc3f9090fdbf5a238a52a75aef453491a
Gabriel Tan <gabtan99@gmail.com>
2019-02-18 14:16:45 +0800

    register apple

--- src/sample/loginController.java
+++ src/sample/loginController.java
@@ -1,5 +1,6 @@
 package sample;
 
+import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.scene.layout.AnchorPane;
 
@@ -8,9 +9,15 @@ public class loginController {
 
 	public AnchorPane registerAnchor;
 	public AnchorPane welcomeAnchor;
+	public JFXButton registerBack;
 
 	public void changeToRegister(ActionEvent actionEvent) {
 		welcomeAnchor.setVisible(false);
 		registerAnchor.setVisible(true);
+	}
+
+	public void changeToWelcome(ActionEvent actionEvent) {
+		registerAnchor.setVisible(false);
+		welcomeAnchor.setVisible(true);
 	}
 }
