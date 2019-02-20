fd3d719748e3527f76432b2db78723d7a274e817
Denzel Co <denz3550@gmail.com>
2019-02-20 19:29:52 +0800

    q

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -21,6 +21,8 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
+        dbConnection connection = new dbConnection();
+        connection.getConnection();
         launch(args);
     }
 }
