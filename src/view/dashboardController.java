bc75e565b0eb506f8a6d7dc0567d2bbd2c5a4bbd
Gabriel Tan <gabtan99@gmail.com>
2019-02-22 10:03:44 +0800

    shortened dahsboard controler

--- src/view/dashboardController.java
+++ src/view/dashboardController.java
@@ -1,5 +1,6 @@
 package view;
 
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXScrollPane;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
@@ -12,42 +13,29 @@ public class dashboardController {
 
 	public JFXScrollPane mainScrollPane;
 	public Text usernameHeader;
+	public JFXButton songsTab;
+	public JFXButton playlistsTab;
+	public JFXButton favoritesTab;
+	public JFXButton profilesTab;
 
-	public void switchSongsPane(ActionEvent actionEvent) throws IOException {
-		clearPanes();
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("songsTabView.fxml"));
-		AnchorPane page = loader.load();
-		mainScrollPane.getChildren().add(page);
-
+	public void changeTab (ActionEvent actionEvent) throws IOException {
+		if (actionEvent.getSource()==songsTab) {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("songsTabView.fxml"));
+			mainScrollPane.getChildren().setAll((AnchorPane) loader.load());
+		}
+		else if (actionEvent.getSource() == playlistsTab) {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("playlistsTabView.fxml"));
+			mainScrollPane.getChildren().setAll((AnchorPane) loader.load());
+		}
+		else if (actionEvent.getSource() == favoritesTab) {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("favoritesTabView.fxml"));
+			mainScrollPane.getChildren().setAll((AnchorPane) loader.load());
+		}
+		else if (actionEvent.getSource() == profilesTab) {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("profilesTabView.fxml"));
+			mainScrollPane.getChildren().setAll((AnchorPane) loader.load());
+		}
 	}
-
-	public void switchPlaylistsPane(ActionEvent actionEvent) throws IOException {
-		clearPanes();
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("playlistsTabView.fxml"));
-		AnchorPane page = loader.load();
-		mainScrollPane.getChildren().add(page);
-
-	}
-
-	public void switchFavoritesPane(ActionEvent actionEvent) throws IOException {
-		clearPanes();
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("favoritesTabView.fxml"));
-		AnchorPane page = loader.load();
-		mainScrollPane.getChildren().add(page);
-	}
-
-	public void switchProfilePane(ActionEvent actionEvent) throws IOException {
-		clearPanes();
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("profilesTabView.fxml"));
-		AnchorPane page = loader.load();
-		mainScrollPane.getChildren().add(page);
-
-	}
-
-	public void clearPanes() {
-		mainScrollPane.getChildren().clear();
-	}
-
 
 
 }
--- src/view/startController.java
+++ src/view/startController.java
@@ -19,26 +19,33 @@ public class startController {
 	public JFXButton guestLoginButton;
 	public JFXScrollPane loginScrollPane;
 	public JFXButton registerBack;
+	public JFXButton registerConfirmButton;
 
+	public void initialize() {
+		System.out.println("youre in login");
+	}
 
 	public void changeToRegister(ActionEvent actionEvent) throws IOException {
 		clearPanes();
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("registerView.fxml"));
 		AnchorPane page = loader.load();
 		loginScrollPane.getChildren().add(page);
+		loader.setController(this);
+
 	}
 
 	public void changeToWelcome(ActionEvent actionEvent) throws IOException {
-		loginScrollPane.getChildren().removeAll();
+		clearPanes();
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("loginView.fxml"));
 		AnchorPane page = loader.load();
 		loginScrollPane.getChildren().add(page);
+		loader.setController(this);
 	}
 
 	public void changeToDashboard(ActionEvent actionEvent) throws Exception {
 		Parent dashboard = FXMLLoader.load(getClass().getResource("dashboardView.fxml"));
 		Stage myStage = (Stage) guestLoginButton.getScene().getWindow();
-		myStage.setScene(new Scene(dashboard, 1000, 600));
+		myStage.setScene(new Scene(dashboard));
 	}
 
 	private void clearPanes() {
