55524ffa31bd27b9d27b6767819fae2e892c3b55
Denzel Co <denz3550@gmail.com>
2019-02-28 17:25:03 +0800

    a

--- src/controller/DashboardController.java
+++ src/controller/DashboardController.java
@@ -1,7 +1,83 @@
 package controller;
 
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import model.Track;
+import org.controlsfx.control.PopOver;
+
 public class DashboardController extends Controller{
 
 	public void playSong() {}
+
+	public void populateSongList(){
+		// Song list renderer
+		for (Track song: parent.songlist){
+			HBox songCell = new HBox();
+			songCell.setSpacing(20);
+
+			ImageView cover = new ImageView(new Image(getClass().getResourceAsStream("/resources/blank_album.jpg")));
+			cover.setFitHeight(40);
+			cover.setFitWidth(40);
+
+
+			Label title = new Label(song.getName());
+			Label artist = new Label ("Artist ");
+			Label duration = new Label ("00:00");
+
+			title.setPrefWidth(250);
+			artist.setPrefWidth(220);
+			duration.setPrefWidth(40);
+
+			duration.setOpacity(0.50);
+
+
+
+			Button play = new Button(" ");
+			play.getStylesheets().add(getClass().getResource("theme.css").toString());
+			play.getStyleClass().add("play-button");
+			play.setMinHeight(32);
+			play.setMinWidth(32);
+
+
+			Button plus = new Button("+");
+
+			plus.setOnMouseClicked(e -> {
+
+				AnchorPane details = new AnchorPane();
+				details.setMinWidth(150);
+				details.setMinHeight(150);
+
+
+				PopOver songDetails = new PopOver();
+
+				songDetails.setArrowLocation(PopOver.ArrowLocation.RIGHT_TOP);
+				songDetails.setContentNode(details);
+				songDetails.setAutoFix(true);
+				songDetails.setAutoHide(true);
+				songDetails.setHideOnEscape(true);
+				songDetails.setDetachable(false);
+				songDetails.show(plus);
+			});
+
+
+			songCell.getChildren().addAll(play, cover, duration, title, artist ,plus);
+			songsListView.getItems().add(songCell);
+
+			songCell.setOnMouseClicked(e -> {
+				if(e.getButton().equals(MouseButton.PRIMARY)){
+					if(e.getClickCount() == 2){
+						System.out.println("Play " + song.getName());
+						parent.sayHi();
+					}
+				}
+			});
+
+		}
+	}
 }
--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -1,4 +1,38 @@
 package controller;
 
+import javafx.animation.PauseTransition;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.io.IOException;
+
 public class LoginController extends Controller {
+
+    public void changeLoginPane(ActionEvent actionEvent) throws IOException {
+        if (actionEvent.getSource() == loginButton) {
+            Parent dashboard = FXMLLoader.load(getClass().getResource("dashboardTemplate.fxml"));
+            Stage myStage = (Stage) loginButton.getScene().getWindow();
+            loginButton.setText("Logging in...");
+            PauseTransition pause = new PauseTransition(Duration.seconds(0.7));
+            pause.setOnFinished(e -> myStage.setScene(new Scene(dashboard)));
+            pause.play();
+        }
+        else if (actionEvent.getSource() == guestLoginButton) {
+            Parent dashboard = FXMLLoader.load(getClass().getResource("dashboardTemplate.fxml"));
+            Stage myStage = (Stage) guestLoginButton.getScene().getWindow();
+            guestLoginButton.setText("Logging in...");
+            PauseTransition pause = new PauseTransition(Duration.seconds(0.7));
+            pause.setOnFinished(e -> myStage.setScene(new Scene(dashboard)));
+            pause.play();
+        }
+        else if (actionEvent.getSource() == accountRegisterButton) {
+            Parent register = FXMLLoader.load(getClass().getResource("registerTemplate.fxml"));
+            Stage myStage = (Stage) accountRegisterButton.getScene().getWindow();
+            myStage.setScene(new Scene(register));
+        }
+    }
 }
