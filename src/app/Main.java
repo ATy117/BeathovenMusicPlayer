ce4cb11f4ee0016dc43058d0f2a1c6ca7ef8903d
rebecabecalyn <45415030+rebecabecalyn@users.noreply.github.com>
2019-03-01 10:29:56 +0800

    renewFxmlTemplates

--- src/app/Main.java
+++ src/app/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/view/LoginTemplate.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/view/loginPageTemplate.fxml"));
         primaryStage.setTitle("Beathoven");
         primaryStage.setScene(new Scene(root));
         primaryStage.setResizable(false);
