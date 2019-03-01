62ad3f40e091bd7e275ac7da5db85cadc28e8ec3
ATy117 <41797980+ATy117@users.noreply.github.com>
2019-03-01 18:15:22 +0800

    dasboard models

--- src/model_rework/Dashboard.java
+++ src/model_rework/Dashboard.java
@@ -1,17 +1,56 @@
 package model_rework;
 
-import dbservice.AlbumDAO;
-import dbservice.PlaylistDAO;
-import dbservice.SongDAO;
+import dbservice.*;
 
 public class Dashboard {
     private User user;
     PlaylistDAO playlistDAO;
     AlbumDAO albumDAO;
-    SongDAO songsDAO;
+    SongDAO songDAO;
 
     public Dashboard (GuestUser GU){
         user = GU;
+        playlistDAO  = new PlaylistDAOLocal();
+        albumDAO = new AlbumDAOLocal();
+        songDAO = new SongDAOLocal();
+    }
 
+    public Dashboard (RegisteredUser RU){
+        user = RU;
+        playlistDAO  = new PlaylistDAODB();
+        albumDAO = new AlbumDAODB();
+        songDAO = new SongDAODB();
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public PlaylistDAO getPlaylistDAO() {
+        return playlistDAO;
+    }
+
+    public void setPlaylistDAO(PlaylistDAO playlistDAO) {
+        this.playlistDAO = playlistDAO;
+    }
+
+    public AlbumDAO getAlbumDAO() {
+        return albumDAO;
+    }
+
+    public void setAlbumDAO(AlbumDAO albumDAO) {
+        this.albumDAO = albumDAO;
+    }
+
+    public SongDAO getSongDAO() {
+        return songDAO;
+    }
+
+    public void setSongDAO(SongDAO songDAO) {
+        this.songDAO = songDAO;
     }
 }
