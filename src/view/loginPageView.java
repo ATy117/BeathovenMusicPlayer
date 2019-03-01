6f4d8f7fc9de3a973141df8ccf2efb774f0d4ba2
rebecabecalyn <45415030+rebecabecalyn@users.noreply.github.com>
2019-03-01 21:02:39 +0800

    templates

--- src/view/loginPageView.java
+++ src/view/loginPageView.java
@@ -1,4 +1,25 @@
 package view;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+
 public class loginPageView {
+    public JFXButton loginBtn;
+
+    public void sayHi(ActionEvent actionEvent) throws IOException{
+        if(actionEvent.getSource() == loginBtn){
+            System.out.println("LogIn!");
+            Parent dashboard = FXMLLoader.load(getClass().getResource("dashboardTemplate.fxml"));
+
+            Stage myStage = (Stage) loginBtn.getScene().getWindow();
+            myStage.setScene(new Scene(dashboard));
+        }
+    }
+
 }
