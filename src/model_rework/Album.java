8f87c010829d956413ead0ab5c7b196dd87a3775
ATy117 <41797980+ATy117@users.noreply.github.com>
2019-03-01 16:27:39 +0800

    new models

---
+++ src/model_rework/Album.java
@@ -0,0 +1,40 @@
+package model_rework;
+
+public class Album {
+    private int album_id;
+    private String name;
+    private int user_id;
+    private String coverURL;
+
+    public int getAlbum_id() {
+        return album_id;
+    }
+
+    public void setAlbum_id(int album_id) {
+        this.album_id = album_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getCoverURL() {
+        return coverURL;
+    }
+
+    public void setCoverURL(String coverURL) {
+        this.coverURL = coverURL;
+    }
+}
---
+++ src/model_rework/Playlist.java
@@ -0,0 +1,40 @@
+package model_rework;
+
+public class Playlist {
+    private int playlist_id;
+    private String name;
+    private boolean favorite;
+    private int user_id;
+
+    public int getPlaylist_id() {
+        return playlist_id;
+    }
+
+    public void setPlaylist_id(int playlist_id) {
+        this.playlist_id = playlist_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isFavorite() {
+        return favorite;
+    }
+
+    public void setFavorite(boolean favorite) {
+        this.favorite = favorite;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+}
