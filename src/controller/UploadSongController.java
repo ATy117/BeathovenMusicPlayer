2571ce28f53f9448d8995ec66e388cbb7a55296e
ATy117 <41797980+ATy117@users.noreply.github.com>
2019-03-04 17:50:53 +0800

    upload song

--- src/controller/UploadSongController.java
+++ src/controller/UploadSongController.java
@@ -1,10 +1,14 @@
 package controller;
 
+import dbservice.AlbumDAO;
+import dbservice.AlbumDAOLocal;
+import dbservice.SongDAO;
+import dbservice.SongDAOLocal;
 import javafx.stage.Stage;
-import model_rework.LibraryModel;
-import model_rework.ProfileModel;
+import model_rework.*;
 import view.UploadSongsView;
 
+import java.io.File;
 import java.sql.Connection;
 
 public class UploadSongController {
@@ -24,7 +28,80 @@ public class UploadSongController {
     }
 
     
-	public boolean confirmDetails() {
+	public boolean uploadSong(String song_title, String artist_name, String album_name, String genre, String year, File file ) {
+		User user = profilemodel.getUser();
+		int user_id = user.getUser_id();
+		SongDAO sd = new SongDAOLocal();
+		AlbumDAO ad = new AlbumDAOLocal();
+
+		if (sd.checkSong(user_id, song_title, artist_name)){
+			System.out.println("Existing");
+		} else {
+			String legit = album_name.replaceAll("[^a-zA-Z]+", "");
+			if (legit.equals("")){
+				SongBuilder builder = new SongBuilder();
+				Song newSong = builder.withAlbumID(-1)
+						.withName(song_title)
+						.withArtistName(artist_name)
+						.withGenre(genre)
+						.withOwner(user_id)
+						.withFavoriteStatus(false)
+						.withTimesPlayed(0)
+						.withFile(file)
+						.withYear(Integer.parseInt(year))
+						.build();
+				sd.addSong(newSong);
+				System.out.println("No Album");
+				return true;
+			} else {
+				int album_id = ad.checkAlbum(user_id, artist_name, album_name);
+				if ( album_id != -1){
+					SongBuilder builder = new SongBuilder();
+					Song newSong = builder
+							.withAlbumID(album_id)
+							.withName(song_title)
+							.withArtistName(artist_name)
+							.withGenre(genre)
+							.withOwner(user_id)
+							.withFavoriteStatus(false)
+							.withTimesPlayed(0)
+							.withFile(file)
+							.withYear(Integer.parseInt(year))
+							.build();
+					sd.addSong(newSong);
+					System.out.println("Existing Album");
+				} else {
+					AlbumBuilder albumBuilder = new AlbumBuilder();
+					Album newAlbum = albumBuilder
+							.withName(album_name)
+							.withArtist(artist_name)
+							.withArtistID(-1)
+							.withFileCover(null)
+							.withOwner(user_id)
+							.build();
+					int newAlbumID = newAlbum.getAlbum_id();
+
+					SongBuilder newbuilder = new SongBuilder();
+					Song song = newbuilder
+							.withAlbumID(newAlbumID)
+							.withName(song_title)
+							.withArtistName(artist_name)
+							.withGenre(genre)
+							.withOwner(user_id)
+							.withFavoriteStatus(false)
+							.withTimesPlayed(0)
+							.withFile(file)
+							.withYear(Integer.parseInt(year))
+							.build();
+
+					ad.addAlbum(newAlbum);
+					sd.addSong(song);
+					System.out.println("New Album");
+					return true;
+				}
+			}
+		}
+		System.out.println(sd.getAllSong(user_id));
 		uploadStage.close();
 		return true;
 	}
