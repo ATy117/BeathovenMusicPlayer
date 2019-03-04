2a343053a006e6615200d49139f6af02bbdbbd19
rebecabecalyn <45415030+rebecabecalyn@users.noreply.github.com>
2019-03-04 16:57:35 +0800

    uploadStuffs

--- src/view/UploadSongsView.java
+++ src/view/UploadSongsView.java
@@ -1,14 +1,28 @@
 package view;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
 import controller.StageManager;
 import controller.UploadSongController;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 public class UploadSongsView{
 
 	Stage primaryStage;
 	UploadSongController controller;
+
+	@FXML public JFXTextField titleField;
+	@FXML public  JFXTextField artistField;
+	@FXML public JFXTextField albumField;
+	@FXML public JFXComboBox genreField;
+	@FXML public JFXTextField yearField;
+	@FXML public JFXButton uploadBtn;
+	@FXML public JFXButton doneBtn;
 
 	public UploadSongsView (Stage primaryStage, UploadSongController controller) {
 
@@ -21,14 +35,34 @@ public class UploadSongsView{
 		StageManager sm = new StageManager(primaryStage);
 		sm.loadScene(loader);
 		sm.setWindowName("Upload songs");
+		init();
 	}
 
 	public void uploadSongFile(javafx.event.ActionEvent actionEvent) {
+	    System.out.println("Upload button!");
 		// file window
 	}
 
 
 	public void addEditSongDetails(javafx.event.ActionEvent actionEvent) {
-		controller.confirmDetails();
+
+	    if(!(titleField.getText().isEmpty() || artistField.getText().isEmpty() || genreField.getValue().toString().isEmpty() || yearField.getText().isEmpty())) {
+	        System.out.println("It is not Empty!");
+	        primaryStage.close();
+		    //controller.confirmDetails(titleField.getText(), artistField.getText(), genreField.getValue(), yearField.getText());
+        }
 	}
+
+	public void init(){
+	    genreField.getItems().add("Pop");
+	    genreField.getItems().add("Rock");
+	    genreField.getItems().add("Country");
+	    genreField.getItems().add("Hip-hop");
+	    genreField.getItems().add("Classic");
+	    genreField.getItems().add("Reggae");
+	    genreField.getItems().add("Ballad");
+	    genreField.getItems().add("EDM");
+	    genreField.getItems().add("Folk");
+    }
+
 }
