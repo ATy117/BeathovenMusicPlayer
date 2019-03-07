6deb6ed70bcc2acc53b26ea0af5f367983c865b8
rebecabecalyn <45415030+rebecabecalyn@users.noreply.github.com>
2019-03-07 12:21:43 +0800

    ShowProfileInfo

--- src/view/ShowProfileView.java
+++ src/view/ShowProfileView.java
@@ -1,11 +1,16 @@
 package view;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
 import controller.StageManager;
 import controller.ShowProfileController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import model_rework.ProfileModel;
 
@@ -20,6 +25,12 @@ public class ShowProfileView extends View{
 	@FXML public JFXButton backBtn;
 	@FXML public JFXButton logoutBtn;
 	@FXML public JFXButton editBtn;
+	@FXML public JFXTextField firstNameField;
+	@FXML public JFXTextField lastnameField;
+	@FXML public JFXTextField usernameField;
+	@FXML public JFXTextField mostPlayedField;
+	@FXML public AnchorPane myProfilePane;
+	@FXML public Circle profilePic;
 
 
 	public ShowProfileView (Stage primaryStage, ProfileModel profilemodel, ShowProfileController controller) {
@@ -34,8 +45,34 @@ public class ShowProfileView extends View{
 		StageManager sm = new StageManager(primaryStage);
 		sm.loadScene(loader);
 		sm.setWindowName("My Profile");
+
+		init();
 	}
 
+	public void init()
+	{
+		//myProfilePane.getStyleClass().add("view/theme.css/.text-field");
+		//myProfilePane.getStylesheets().add("view/theme.css");
+
+		if(profilemodel.getUser().getAvatarURL() == null) {
+			Image defaultPic = new Image("resources/user.png");
+			profilePic.setFill(new ImagePattern(defaultPic));
+		}
+		else{
+			Image userPic = new Image(profilemodel.getUser().getAvatarURL().toString());
+			profilePic.setFill(new ImagePattern(userPic));
+		}
+
+		firstNameField.setText(profilemodel.getUser().getFirst_name());
+		lastnameField.setText(profilemodel.getUser().getLast_name());
+		usernameField.setText(profilemodel.getUser().getUsername());
+		mostPlayedField.setText("0");
+
+		firstNameField.setEditable(false);
+		lastnameField.setEditable(false);
+		usernameField.setEditable(false);
+		mostPlayedField.setEditable(false);
+	}
 
 
 
@@ -52,5 +89,20 @@ public class ShowProfileView extends View{
 
 	public void editUserDetails(ActionEvent actionEvent) {
 		controller.editUserDetails();
+
+		if(editBtn.getText().equals("edit"))
+		{
+			editBtn.setText("done");
+			firstNameField.setEditable(true);
+			lastnameField.setEditable(true);
+			usernameField.setEditable(true);
+		}
+		else if(editBtn.getText().equals("done"))
+		{
+			firstNameField.setEditable(false);
+			lastnameField.setEditable(false);
+			usernameField.setEditable(false);
+		}
+
 	}
 }
