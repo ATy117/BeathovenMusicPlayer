ef3602a490e700f7c57d9100399551559d6ec9e0
Gabriel Tan <gabtan99@gmail.com>
2019-03-14 08:06:08 +0800

    edited sql file

--- cleanbeathovendb.sql
+++ cleanbeathovendb.sql
@@ -1,3 +1,6 @@
+CREATE DATABASE  IF NOT EXISTS `beathovendb` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `beathovendb`;
+
 -- MySQL dump 10.13  Distrib 8.0.15, for macos10.14 (x86_64)
 --
 -- Host: localhost    Database: beathovendb
